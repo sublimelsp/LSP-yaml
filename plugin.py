821f61e7f1b752ee5e780a8d29933a8647abfcb7
Jack Cherng <jfcherng@gmail.com>
2024-04-20 04:50:36 +0800

    refactor: use py38 features
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -1,17 +1,20 @@
-from LSP.plugin.core.typing import Callable
-from lsp_utils import NpmClientHandler
+from __future__ import annotations
+
 import functools
 import os
-import sublime
 import urllib.parse
 import urllib.request
+from collections.abc import Callable
+
+import sublime
+from lsp_utils import NpmClientHandler
 
 
-def plugin_loaded():
+def plugin_loaded() -> None:
     LspYamlPlugin.setup()
 
 
-def plugin_unloaded():
+def plugin_unloaded() -> None:
     LspYamlPlugin.cleanup()
 
 
@@ -19,13 +22,14 @@ class LspYamlPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = "language-server"
     server_binary_path = os.path.join(
-        server_directory, "node_modules", "yaml-language-server", "bin", "yaml-language-server"
+        server_directory,
+        "node_modules",
+        "yaml-language-server",
+        "bin",
+        "yaml-language-server",
     )
 
     def on_open_uri_async(self, uri: str, callback: Callable[[str, str, str], None]) -> bool:
-        if not uri.startswith("json-schema:"):
-            return False
-
         def run_blocking() -> None:
             title = urllib.parse.urldefrag(uri).url
             try:
@@ -34,11 +38,15 @@ class LspYamlPlugin(NpmClientHandler):
                 http_url = urllib.parse.unquote(parsed.fragment)
                 with urllib.request.urlopen(http_url) as f:
                     content = f.read().decode("utf-8").replace("\r", "")
-                syntax = sublime.find_syntax_for_file(urllib.parse.urlparse(http_url).path).path
+                if syntax_obj := sublime.find_syntax_for_file(urllib.parse.urlparse(http_url).path):
+                    syntax = syntax_obj.path
             except Exception as ex:
-                content = "Error: {}".format(ex)
+                content = f"Error: {ex}"
                 syntax = "Packages/Text/Plain text.tmLanguage"
             sublime.set_timeout_async(functools.partial(callback, title, content, syntax))
+
+        if not uri.startswith("json-schema:"):
+            return False
 
         sublime.set_timeout_async(run_blocking)
         return True
