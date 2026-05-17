a415d62bb5e37bdf3dd9afb1ea085bfe20c423f4
Rafal Chlodnicki <rchl2k@gmail.com>
2026-05-17 22:56:51 +0200

    nicer title

--- plugin.py
+++ plugin.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import final
-from urllib.parse import unquote, urldefrag, urlparse
+from urllib.parse import unquote, urlparse
 from urllib.request import urlopen
 
 import sublime
@@ -40,16 +40,15 @@ class LspYamlPlugin(LspPlugin):
     def handle_json_schema_uri(self, uri: DocumentUri, flags: sublime.NewFileFlags) -> Promise[sublime.Sheet | None]:
         if not (session := self.weaksession()):
             return Promise.resolve(None)
-        title = urldefrag(uri).url
+        parsed = urlparse(uri)
+        http_url = unquote(parsed.fragment)
         syntax = "Packages/Text/Plain text.tmLanguage"
         try:
-            parsed = urlparse(uri)
-            http_url = unquote(parsed.fragment)
             with urlopen(http_url) as f:
                 content = f.read().decode("utf-8").replace("\r", "")
             if syntax_obj := sublime.find_syntax_for_file(urlparse(http_url).path):
                 syntax = syntax_obj.path
         except Exception as ex:
             content = f"Error: {ex}"
-        return session.open_scratch_buffer(title, content, syntax, uri, None, flags) \
+        return session.open_scratch_buffer(http_url, content, syntax, uri, None, flags) \
             .then(lambda view: view.sheet() if view else None)
